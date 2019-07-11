e3ab7b2f373afbf9f5807c41e88972b2ba1d516e
shj112233 <74210847@qq.com>
2019-07-11 21:34:09 +0800

    登录完成

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class='login-container'>
 
-    <el-card class="login-box">
+    <el-form class="login-box">
       <img src="../../assets/images/logo_index.png"
            alt="">
       <!--验证表单  ！-->
@@ -14,8 +14,8 @@
                     placeholder="请输入手机号"></el-input>
         </el-form-item>
         <el-form-item prop="code">
-          <el-input v-model="loginForm.mobile"
-                    placeholder="请输入密码"
+          <el-input v-model="loginForm.code"
+                    placeholder="请输入验证码"
                     style="width:240px"></el-input>
           <el-button style="float:right">发送验证码</el-button>
 
@@ -25,15 +25,25 @@
         </el-form-item>
         <el-form-item>
           <el-button style="width:100%"
-                     type="primary">登录</el-button>
+                     type="primary"
+                     @click="login()">登录</el-button>
         </el-form-item>
       </el-form>
-    </el-card>
+    </el-form>
   </div>
 </template>
 <script>
 export default {
   data () {
+    // 自定义校验手机号
+    const checkMobile = (rule, value, callback) => {
+      // 校验逻辑  把value拿出来进行手机号的格式校验
+      if (/^1[3-9]\d{9}$/.test(value)) {
+        callback()
+      } else {
+        callback(new Error('手机号格式不正确'))
+      }
+    }
     return {
       // 表单数据对象
       loginForm: {
@@ -43,11 +53,13 @@ export default {
       // 表单验证规则对象
       loginRules: {
         mobile: [
-          { required: true, message: '请输入手机号码', trigger: 'blur' }
+          { required: true, message: '请输入手机号码', trigger: 'blur' },
+          { validator: checkMobile, trigger: 'blur' }
 
         ],
         code: [
-          { required: true, message: '请输入验证码', trigger: 'blur' }
+          { required: true, message: '请输入验证码', trigger: 'blur' },
+          { len: 6, message: '请输入6位数字', trigger: 'blur' }
         ]
       }
     }
@@ -60,6 +72,17 @@ export default {
           // 提交登录请求 接口使用文档  axios 是基于promise封装的post（） 返回一个promise对象
           this.$http
             .post('http://ttapi.research.itcast.cn/mp/v1_0/authorizations', this.loginForm)
+            .then(res => {
+              // res是  响应对象  包含 后台返回的数据 res.data
+              // consol.log(res.data)  去做什么事情？？？
+              // 1 跳转到首页
+              // TODO 2 保存用户的信息  用来判断登录的状态
+              this.$router.push('/')
+            })
+            .catch(() => {
+              // 提示
+              this.$message.error('手机号或验证码错误')
+            })
         }
       })
     }
