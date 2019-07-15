a860425761187cd0a37b5da148e2445b7c61b7b8
shj112233 <74210847@qq.com>
2019-07-15 09:54:07 +0800

    文章列表 表单 筛选功能

--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -8,11 +8,47 @@
           <el-breadcrumb-item>内容管理</el-breadcrumb-item>
         </el-breadcrumb>
       </div>
+      <!-- 筛选表单 -->
+      <el-form :model="reqParams"
+               size="small"
+               label-width="80px">
+        <el-form-item label="状态：">
+          <el-radio-group v-model="reqPatams.status">
+            <el-radio :label="null">全部</el-radio>
+            <el-radio :label="0">草稿</el-radio>
+            <el-radio :label="1">待审核</el-radio>
+            <el-radio :label="2">审核通过</el-radio>
+            <el-radio :label="3">审核失败</el-radio>
+          </el-radio-group>
+        </el-form-item>
+        <el-form-item label="频道：">
+          <el-select v-model="reqPatams.channel_id"
+                     placeholder="所有频道">
+            <el-option v-for="item in channelOptions"
+                       :key="item.value"
+                       :label="item.label"
+                       :value="item.value">
+            </el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="时间：">
+          <div class="block">
+            <el-date-picker v-model="datavalues"
+                            type="daterange"
+                            range-separator="至"
+                            start-placeholder="开始日期"
+                            end-placeholder="结束日期">
+            </el-date-picker>
+          </div>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary">筛选</el-button>
+        </el-form-item>
+      </el-form>
     </el-card>
     <!-- 结果区域 -->
     <el-card></el-card>
   </div>
-  <!-- 筛选区域 -->
 
   <!-- 结果区域 -->
 
@@ -21,6 +57,33 @@
 <script>
 
 export default {
+  data () {
+    return {
+      // 提交后台的筛选条件数据
+      reqPatams: {
+        //  后端接口文档中的数据时status  是属于reqParams的
+        // 默认数据 ""  与 null区别
+        //  如果是 null 该字段 是不会提交给后台的  这个是后端要求不传 ，空字符串 也是要传的
+        status: null,
+        channel_id: null,
+        begin_pubdate: null,
+        end_pubdate: null
+      },
+      // 频道的选项数据
+      channelOptions: [{
+        value: '选项1',
+        label: '黄金糕'
+      }, {
+        value: '选项2',
+        label: '双皮奶'
+      }, {
+        value: '选项3',
+        label: '蚵仔煎'
+      }],
+      //  日期数据  为了绑定begin和end两项数据
+      datavalues: []
+    }
+  }
 
 }
 </script>
